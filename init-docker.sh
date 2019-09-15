6980b06c9379e9dce778722d9bf3153a064f5a06
Filipe Shigueru <fshigueru@fshigueru.fshigueru.net>
2019-09-14 21:28:38 -0300

    init docker

--- init-docker.sh
+++ init-docker.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
-docker build -t fshigueru/php7.1 php/7.1/.
-docker build -t fshigueru/nginx nginx/.
-docker build -t fshigueru/redis redis/.
+cd api.restaurant
+touch .env
+echo "PROJECT_NAME=api.restaurant" >> .env
+echo "MYSQL_ROOT_PASSWORD=root" >> .env
+echo "MYSQL_DATABASE=hackapi" >> .env
+echo "MYSQL_USER=hackapi" >> .env
+echo "MYSQL_PASSWORD=hackapi" >> .env
+echo "DB_HOST=mysql" >> .env
+
+docker network create hackathon
+
+make up
+
+cd src
+
+export fileid=1sHcJdYAASvBO3P4h4pvBOegusQIgv7RK
+export filename=.env
+
+wget -O $filename 'https://docs.google.com/uc?export=download&id='$fileid
+
+cd ..
+
+make composer_install
+
+make migrate
